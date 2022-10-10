74dd971ea79dccde5d9a4230df6a1a953fb6a853
Maulana Fatullah <fistalanid@gmail.com>
2022-10-10 20:43:36 +0700

    nav mobile dropdown issue

--- inBeach/assets/js/nav-dropdown.js
+++ inBeach/assets/js/nav-dropdown.js
@@ -1,13 +1,12 @@
+const btnNavMobile = document.querySelector('.hamburger-menu');
+const btnCloseNavMobile = document.querySelector('.close-btn');
+const navMobile = document.querySelector('#nav-mobile');
 const desktopIslandDropdown = document.querySelector('.desktop-island-dropdown');
 const desktopFnbDropdown = document.querySelector('.desktop-fnb-dropdown');
+const ulSubIsland = document.querySelector('ul.sub-island');
 const subIsland = document.querySelector('.sub-island');
 const subFnb = document.querySelector('.sub-fnb');
 const main = document.querySelector('main');
-
-const restaurant = document.querySelector('.restaurant');
-const restaurantLocation = document.querySelector('.restaurant-location');
-const cafe = document.querySelector('.cafe');
-const cafeLocation = document.querySelector('.cafe-location');
 
 const styleEl = document.createElement('style');
 document.head.appendChild(styleEl);
@@ -40,13 +39,13 @@ window.addEventListener('load', function () {
         const sublistLength = document.querySelectorAll('#sublist-number-' + index + '>ul>li').length
 
         const id = document.getElementById('dropdown-number-' + index);
-        const x = document.getElementById('dropdown-number-' + (index + 1));
+        // const x = document.getElementById('dropdown-number-' + (index + 1));
         id.addEventListener('click', function () {
             if (document.getElementById('dropdown-number-' + index).classList.contains('dropdown-list-size') || document.getElementById('sublist-number-' + index).classList.contains('dropdown-animation-list')) {
                 document.querySelector('.dropdown-list>ul>li[class~="dropdown-list-size"]').classList.remove('dropdown-list-size');
                 document.querySelector('.dropdown-list>ul>li>div[class~="dropdown-animation-list"]').classList.remove('dropdown-animation-list');
             } else {
-                if (sessionStorage.getItem(dropdownKey) === '') {
+                if (sessionStorage.getItem(dropdownKey) === '' && sessionStorage.getItem(sublistKey) === '') {
                     sessionStorage.setItem(dropdownKey, 'dropdown-number-' + index);
                     sessionStorage.setItem(sublistKey, 'sublist-number-' + index);
                 } else {
@@ -86,15 +85,16 @@ main.addEventListener('click', function () {
             desktopIslandDropdown.removeEventListener('webkitAnimationEnd', arguments.callee, false);
         }, false);
     }
-    restaurantLocation.classList.remove('restaurant-location-dropdown');
-    restaurant.classList.remove('restaurant-dropdown');
-    cafeLocation.classList.remove('cafe-location-dropdown');
-    cafe.classList.remove('cafe-dropdown');
-    document.querySelector('.dropdown-list>ul>li[class~="dropdown-list-size"]').classList.remove('dropdown-list-size');
-    document.querySelector('.dropdown-list>ul>li>div[class~="dropdown-animation-list"]').classList.remove('dropdown-animation-list');
+    if (!document.getElementById(sessionStorage.getItem(dropdownKey)).classList.contains('dropdown-list-size') || !document.getElementById(sessionStorage.getItem(sublistKey)).classList.contains('dropdown-animation-list')) {
+        document.getElementById(sessionStorage.getItem(dropdownKey)).classList.remove('dropdown-list-size');
+        document.getElementById(sessionStorage.getItem(sublistKey)).classList.remove('dropdown-animation-list');
+    }
+    if (styleSheet.cssRules.length > 1) {
+        styleSheet.deleteRule(0);
+        styleSheet.deleteRule(0);
+    }
+    navMobile.classList.remove('active');
 
-    styleSheet.deleteRule(0);
-    styleSheet.deleteRule(0);
 });
 
 subFnb.addEventListener('click', function () {
@@ -132,26 +132,15 @@ subIsland.addEventListener('click', () => {
             desktopFnbDropdown.removeEventListener('webkitAnimationEnd', arguments.callee, false);
         }, false);
     }
-    restaurantLocation.classList.remove('restaurant-location-dropdown');
-    restaurant.classList.remove('restaurant-dropdown');
 
     if (styleEl.sheet.cssRules.length == 0) {
         styleSheet.insertRule('#nav-desktop a[href="#islands"] > span::before { transition: .4s;transform: translateY(17px) translateX(-7px) rotate(135deg); }', 0);
         styleSheet.insertRule('#nav-desktop a[href="#islands"] > span::after { transform: translateX(-23px) translateY(2px) rotate(45deg); }', 0);
     }
 });
-
-// restaurant.addEventListener('click', function () {
-//     restaurantLocation.classList.add('restaurant-location-dropdown');
-//     restaurant.classList.add('restaurant-dropdown');
-
-//     cafeLocation.classList.remove('cafe-location-dropdown');
-//     cafe.classList.remove('cafe-dropdown');
-// });
-// cafe.addEventListener('click', function () {
-//     cafeLocation.classList.add('cafe-location-dropdown');
-//     cafe.classList.add('cafe-dropdown');
-
-//     restaurantLocation.classList.remove('restaurant-location-dropdown');
-//     restaurant.classList.remove('restaurant-dropdown');
-// });
+btnNavMobile.addEventListener('click', function () {
+    navMobile.classList.add('active');
+});
+btnCloseNavMobile.addEventListener('click', function () {
+    navMobile.classList.remove('active');
+});
