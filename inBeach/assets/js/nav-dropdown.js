1277e27505d986100f316c4a4799e2b7e9e06f2c
Maulana Fatullah <fistalanid@gmail.com>
2022-10-15 18:19:43 +0700

    desktop dropdown dynamic test --v4

--- inBeach/assets/js/nav-dropdown.js
+++ inBeach/assets/js/nav-dropdown.js
@@ -6,27 +6,35 @@ const desktopFnbDropdown = document.querySelector('.desktop-fnb-dropdown');
 const ulSubIsland = document.querySelector('ul.sub-island');
 const subIsland = document.querySelector('.sub-island');
 const subFnb = document.querySelector('.sub-fnb');
+const divSubNav = document.querySelectorAll('#nav-desktop div.sub-nav');
+const navDesktopContainer = document.querySelectorAll('#nav-desktop > div > div');
 const main = document.querySelector('main');
 
 const styleEl = document.createElement('style');
 document.head.appendChild(styleEl);
 const styleSheet = styleEl.sheet;
+
 const dropdownKey = 'DROPDOWN_KEY';
 const sublistKey = 'SUBLIST_KEY';
 const subNavMobileKey = 'SUBNAV_MOBILE_KEY';
 const ulSubNavMobileKey = 'UL_SUBNAV_MOBILE_KEY';
+const divSubNavDesktopKey = 'DIV_SUBNAV_DESKTOP_KEY';
+const desktopContainerNumber = 'DESKTOP_CONTAINER_KEY';
 
+const divDropdownList = document.querySelectorAll('#nav-desktop .dropdown-list');
 const dropdownList = document.querySelectorAll('.dropdown-list>ul>li');
 const dropdownSubList = document.querySelectorAll('.dropdown-list>ul>li>div');
-const subNav = document.querySelectorAll('#nav-mobile div.sub-nav > a');
+const subNavAnchor = document.querySelectorAll('#nav-mobile div.sub-nav > a');
 const ulSubNav = document.querySelectorAll('#nav-mobile ul.sub-nav');
 
 window.addEventListener('load', function () {
-    if (sessionStorage.getItem(dropdownKey) === null || sessionStorage.getItem(sublistKey) === null || sessionStorage.getItem(subNavMobileKey) === null || sessionStorage.getItem(ulSubNavMobileKey) === null) {
+    if (sessionStorage.getItem(dropdownKey) === null || sessionStorage.getItem(sublistKey) === null || sessionStorage.getItem(subNavMobileKey) === null || sessionStorage.getItem(ulSubNavMobileKey) === null || sessionStorage.getItem(divSubNavDesktopKey) === null || sessionStorage.getItem(desktopContainerNumber) === null) {
         sessionStorage.setItem(dropdownKey, '');
         sessionStorage.setItem(sublistKey, '');
         sessionStorage.setItem(subNavMobileKey, '');
         sessionStorage.setItem(ulSubNavMobileKey, '');
+        sessionStorage.setItem(divSubNavDesktopKey, '');
+        sessionStorage.setItem(desktopContainerNumber, '');
     }
     for (let a of dropdownList) {
         a.id = 'dropdown-number-';
@@ -34,22 +42,75 @@ window.addEventListener('load', function () {
     for (const b of dropdownSubList) {
         b.id = 'sublist-number-';
     }
-    for (const c of subNav) {
+    for (const c of subNavAnchor) {
         c.id = 'sub-nav-number-';
     }
     for (const d of ulSubNav) {
         d.id = 'ul-sub-nav-number-';
+    }
+    for (const e of divSubNav) {
+        e.id = 'div-sub-nav-number-';
+    }
+    for (const f of navDesktopContainer) {
+        f.id = 'nav-desktop-container-number-';
+    }
+
+    for (let index = 1; index <= divSubNav.length; index++) {
+        document.getElementById('div-sub-nav-number-').id = 'div-sub-nav-number-' + index;
+    }
+    for (let index = 1; index <= navDesktopContainer.length; index++) {
+        document.getElementById('nav-desktop-container-number-').id = 'nav-desktop-container-number-' + index;
+    }
+
+    for (let index = 1; index <= divSubNav.length; index++) {
+
+        const idDivSubNav = document.getElementById('div-sub-nav-number-' + index);
+
+        idDivSubNav.addEventListener('click', function () {
+
+            if (sessionStorage.getItem(divSubNavDesktopKey) === '') {
+                sessionStorage.setItem(divSubNavDesktopKey, 'div-sub-nav-number-' + index);
+                sessionStorage.setItem(desktopContainerNumber, 'nav-desktop-container-number-' + index);
+            } else {
+                if (styleEl.sheet.cssRules.length > 1) {
+                    styleSheet.deleteRule(0);
+                    styleSheet.deleteRule(0);
+                }
+                const d = document.getElementById('nav-desktop-container-number-' + index);
+
+                if (sessionStorage.getItem(desktopContainerNumber) == d.id) {
+                    d.style.display = 'flex';
+                } else if (sessionStorage.getItem(desktopContainerNumber) != d.id) {
+                    d.style.display = 'flex';
+
+                    const x = document.getElementById(sessionStorage.getItem(desktopContainerNumber));
+                    x.classList.add('close-dropdown');
+                    x.addEventListener('webkitAnimationEnd', function () {
+                        x.style.display = 'none';
+                        x.classList.remove('close-dropdown');
+                        x.removeEventListener('webkitAnimationEnd', arguments.callee, false);
+                    }, false);
+                    sessionStorage.setItem(divSubNavDesktopKey, 'div-sub-nav-number-' + index);
+                    sessionStorage.setItem(desktopContainerNumber, 'nav-desktop-container-number-' + index);
+                } 
+
+                if (styleEl.sheet.cssRules.length == 0) {
+                    styleSheet.insertRule('#nav-desktop > #' + sessionStorage.getItem(divSubNavDesktopKey) + '> a > span::before { transition: .4s;transform: translateY(17px) translateX(-7px) rotate(135deg); }', 0);
+                    styleSheet.insertRule('#nav-desktop > #' + sessionStorage.getItem(divSubNavDesktopKey) + '> a > span::after { transform: translateX(-23px) translateY(2px) rotate(45deg); }', 0);
+                }
+            }
+        });
     }
 
     for (let index = 1; index <= dropdownList.length; index++) {
         document.getElementById('dropdown-number-').id = 'dropdown-number-' + index;
         document.getElementById('sublist-number-').id = 'sublist-number-' + index;
     }
-    for (let v = 1; v <= subNav.length; v++) {
+    for (let v = 1; v <= subNavAnchor.length; v++) {
         document.getElementById('sub-nav-number-').id = 'sub-nav-number-' + v;
         document.getElementById('ul-sub-nav-number-').id = 'ul-sub-nav-number-' + v;
     }
-    for (let v = 1; v <= subNav.length; v++) {
+    for (let v = 1; v <= subNavAnchor.length; v++) {
         const subNumber = document.getElementById('sub-nav-number-' + v);
 
         subNumber.addEventListener('click', function () {
@@ -64,6 +125,8 @@ window.addEventListener('load', function () {
             }
         });
     }
+
+
     for (let index = 1; index <= dropdownList.length; index++) {
         // const dropdownListSize = document.querySelector('.dropdown-list-size');
         // document.querySelector('.dropdown-list-size').style.height = '5em'
@@ -126,48 +189,6 @@ main.addEventListener('click', function () {
     }
     navMobile.classList.remove('active');
 
-});
-
-subFnb.addEventListener('click', function () {
-    desktopFnbDropdown.style.display = 'flex';
-    if (desktopIslandDropdown.style.display == 'flex') {
-        if (styleEl.sheet.cssRules.length > 1) {
-            styleSheet.deleteRule(0);
-            styleSheet.deleteRule(0);
-        }
-        desktopIslandDropdown.classList.add('close-dropdown');
-        desktopIslandDropdown.addEventListener('webkitAnimationEnd', function () {
-            desktopIslandDropdown.style.display = 'none';
-            desktopIslandDropdown.classList.remove('close-dropdown');
-            desktopIslandDropdown.removeEventListener('webkitAnimationEnd', arguments.callee, false);
-        }, false);
-    }
-
-    if (styleEl.sheet.cssRules.length == 0) {
-        styleSheet.insertRule('#nav-desktop a[href="#fnb"] > span::before { transition: .4s;transform: translateY(17px) translateX(-7px) rotate(135deg); }', 0);
-        styleSheet.insertRule('#nav-desktop a[href="#fnb"] > span::after { transform: translateX(-23px) translateY(2px) rotate(45deg); }', 0);
-    }
-});
-
-subIsland.addEventListener('click', () => {
-    desktopIslandDropdown.style.display = 'flex';
-    if (desktopFnbDropdown.style.display == 'flex') {
-        if (styleEl.sheet.cssRules.length > 1) {
-            styleSheet.deleteRule(0);
-            styleSheet.deleteRule(0);
-        }
-        desktopFnbDropdown.classList.add('close-dropdown');
-        desktopFnbDropdown.addEventListener('webkitAnimationEnd', function () {
-            desktopFnbDropdown.style.display = 'none';
-            desktopFnbDropdown.classList.remove('close-dropdown');
-            desktopFnbDropdown.removeEventListener('webkitAnimationEnd', arguments.callee, false);
-        }, false);
-    }
-
-    if (styleEl.sheet.cssRules.length == 0) {
-        styleSheet.insertRule('#nav-desktop a[href="#islands"] > span::before { transition: .4s;transform: translateY(17px) translateX(-7px) rotate(135deg); }', 0);
-        styleSheet.insertRule('#nav-desktop a[href="#islands"] > span::after { transform: translateX(-23px) translateY(2px) rotate(45deg); }', 0);
-    }
 });
 
 // Nav Mobile
