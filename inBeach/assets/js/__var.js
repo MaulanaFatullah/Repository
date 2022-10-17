cd4303452e207d289295bc764b864ae5776e0fbd
Maulana Fatullah <fistalanid@gmail.com>
2022-10-17 18:24:41 +0700

    file and code maintenance

---
+++ inBeach/assets/js/__var.js
@@ -0,0 +1,23 @@
+export const dropdownKey = 'DROPDOWN_KEY';
+export const sublistKey = 'SUBLIST_KEY';
+export const ulSubNavMobileKey = 'UL_SUBNAV_MOBILE_KEY';
+export const desktopContainerNumber = 'DESKTOP_CONTAINER_KEY';
+
+export const dropdownList = document.querySelectorAll('.dropdown-list>ul>li');
+export const dropdownSubList = document.querySelectorAll('.dropdown-list>ul>li>div');
+export const subNavAnchor = document.querySelectorAll('#nav-mobile div.sub-nav > a');
+export const ulSubNav = document.querySelectorAll('#nav-mobile ul.sub-nav');
+
+export const btnNavMobile = document.querySelector('.hamburger-menu');
+export const btnCloseNavMobile = document.querySelector('.close-btn');
+export const navMobile = document.querySelector('#nav-mobile');
+export const divSubNav = document.querySelectorAll('#nav-desktop div.sub-nav');
+export const navDesktopContainer = document.querySelectorAll('#nav-desktop > div > div');
+export const main = document.querySelector('main');
+
+export const __containerNumber = 'nav-desktop-container-number-';
+export const __dropdownNumber = 'dropdown-number-';
+export const __sublistNumber = 'sublist-number-';
+export const __subNavNumber = 'sub-nav-number-';
+export const __ulSubNavNumber = 'ul-sub-nav-number-';
+export const __divSubNavNumber = 'div-sub-nav-number-';
